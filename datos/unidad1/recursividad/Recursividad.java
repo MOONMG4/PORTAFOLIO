e8aab1ae6487824980e03458b507d781ef965c64
MOONMG4 <moonb9517@gmail.com>
2026-09-24 15:56:04 -0600

    Recursividad imprimiendo 10 veces Hola con nombre

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -1,16 +1,21 @@
 package datos.unidad1.recursividad;
+import java.util.Scanner;
 
 public class Recursividad {
- public static void saludo(int total){
+ public static void saludo(int total, String nombre){
   if(total <= 0)//Condición base
      return;
   else{//Condición recurciva o repititiva
-   System.out.println("Hola");
-   saludo(total-1);
+   System.out.println("Hola " + nombre);
+   saludo(total-1, nombre);
   }
  }
 
  public static void main(String[] args){
-  saludo(10);
+  Scanner sc = new Scanner(System.in);
+
+  System.out.println("Cúal es tu nombre ");
+  String nombre = sc.nextLine();
+  saludo(10, nombre);
  }
 }
